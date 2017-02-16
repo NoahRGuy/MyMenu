cc2a0610f692a8d482d6d89e3463c9b7628e3543
Noah Guy <noahriguy@gmail.com>
2017-02-16 16:47:01 -0600

    write html files, fix most errors

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,23 +2,23 @@ class ItemsController < ApplicationController
 	def new
 		authorize
 		@item = Item.new
-		@menu = Menu.find_by(id: params[:id])
+		@menu = Menu.find_by(id: params[:menu_id])
 	end
 
 	def create
 		authorize
-		@item = Item.new(article_params)
-		@menu = Menu.find_by(id: params[:id])
+		@item = Item.new(item_params)
+		@menu = Menu.find_by(id: params[:menu_id])
 		@item.menu = @menu
 		if @item.save
-			redirect_to menu
+			redirect_to menu_path(@menu)
 		else
 			render 'new'
 		end
 	end
 
 	private
-	def menu_item_params
-		params.require(:article).permit(:title, :description, :price)
+	def item_params
+		params.require(:item).permit(:title, :description, :price, :menu_id)
 	end
 end
