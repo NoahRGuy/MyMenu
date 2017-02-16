09c5c733f8ece1985b0c60e18aec1db1edb4265b
Noah Guy <noahriguy@gmail.com>
2017-02-16 13:33:18 -0600

    write controllers, layout page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:new, :create]
+  resources :menus, only: [:new, :create, :show, :index]
+  resources :menu_items, only: [:new, :create]
 end
